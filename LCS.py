c32f2f344f3f0f8548a7aec598434896152817ef
Karanj Meghpara <karanjmeghpara30@gmail.com>
2026-09-06 16:39:39 +0530

    Refactor LCS.py by removing comments
    Removed comments from the LCS function to improve code clarity.

--- LCS.py
+++ LCS.py
@@ -2,10 +2,9 @@ def lcs(x, y):
     m = len(x)
     n = len(y)
 
-    # Create DP table
     lcs_table = [[0] * (n + 1) for _ in range(m + 1)]
 
-    # Fill the DP table
+    
     for i in range(m + 1):
         for j in range(n + 1):
 
@@ -21,16 +20,12 @@ def lcs(x, y):
                     lcs_table[i][j - 1]
                 )
 
-    # Length of LCS
     index = lcs_table[m][n]
-
-    # Store LCS characters
     lcs_chars = [""] * index
 
     i = m
     j = n
 
-    # Construct LCS by tracing backwards
     while i > 0 and j > 0:
 
         if x[i - 1] == y[j - 1]:
@@ -48,8 +43,6 @@ def lcs(x, y):
 
     return "".join(lcs_chars)
 
-
-# Main program
 x = input("Enter first sequence: ")
 y = input("Enter second sequence: ")
 
